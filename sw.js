398d11c9bf0cb300c0a69f700c9bb10f77dbf4d2
INWLY <inwly@qq.com>
2026-09-02 22:03:50 +0800

    Replace about and tags with blog

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  * Register service worker.
  * ========================================================== */
 
-const PRECACHE = 'precache-v1';
+const PRECACHE = 'precache-v2';
 const RUNTIME = 'runtime';
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
